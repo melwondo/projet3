74e89a68c0b1f27094371e665681a29698eda335
Manuel Konrath <manuel.konrath@gmail.com>
2019-06-04 15:41:14 +0200

    menu dynamique dans la navbar

---
+++ src/Controller/MenuController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Service;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class MenuController extends AbstractController
+{
+    /**
+     * @Route("/menu", name="menu")
+     */
+    public function index()
+    {
+        $services = $this->getDoctrine()
+            ->getRepository(Service::class)
+            ->findBy(['visible'=>1]);
+
+
+        return $this->render('navbar.html.twig', [
+            'services' => $services,
+        ]);
+    }
+}
