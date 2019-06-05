bcfabecb2a7e2a11bea25b680da677f6f477d6d7
giroz <melanie>
2019-06-05 15:41:07 +0200

    appel base service

--- src/Controller/AccueilController.php
+++ src/Controller/AccueilController.php
@@ -5,6 +5,8 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Repository\ServiceRepository;
+use App\Entity\DetailService;
+use App\Repository\DetailServiceRepository;
 
 class AccueilController extends AbstractController
 {
@@ -17,6 +19,21 @@ class AccueilController extends AbstractController
 
         return $this->render('accueil/index.html.twig', [
             'services' => $services,
+        ]);
+    }
+
+
+    /**
+     * @Route("/accueil/{id}", name="accueil_service")
+     */
+    public function detail(DetailService $service)
+    {
+        $detail_service = $service->findBy(['id' => $service->getId()]);
+
+        return $this->render('services/details.html.twig', [
+            'controller_name' => 'ServicesController',
+            'details' => $detail_service,
+            'service' => $service
         ]);
     }
 }
