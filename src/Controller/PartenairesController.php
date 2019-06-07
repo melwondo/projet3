40ac8e24e1085160a99ce409e7db6f3f4c91835f
man0ni <manon.humbert@hotmail.fr>
2019-06-07 10:20:03 +0200

    ajout images partenaires

--- src/Controller/PartenairesController.php
+++ src/Controller/PartenairesController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\PartenaireRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -10,10 +11,11 @@ class PartenairesController extends AbstractController
     /**
      * @Route("/partenaires", name="partenaires")
      */
-    public function index()
+    public function index(PartenaireRepository $partenaires)
     {
+        $partenaires = $partenaires->findAll();
         return $this->render('partenaires/index.html.twig', [
-            'controller_name' => 'PartenairesController',
+            'partenaires' => $partenaires,
         ]);
     }
 }
