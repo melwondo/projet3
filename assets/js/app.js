d49d6091ecb8dbdfc88e074b9149f9f5d3a65d5d
man0ni <manon.humbert@hotmail.fr>
2019-06-14 12:01:48 +0200

    ajout menu backoffice probleme js dans service

--- assets/js/app.js
+++ assets/js/app.js
@@ -12,3 +12,6 @@ require('../scss/app.scss');
 // const $ = require('jquery');
 
 console.log('Hello Webpack Encore! Edit me in assets/js/app.js');
+
+var $ = require('jquery');
+global.$ = global.jQuery = $
