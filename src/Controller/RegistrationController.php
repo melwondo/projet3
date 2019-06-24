7057aa8983a19feccf1ba7e3c3ae7a9f1fdf3924
giroz <melanie>
2019-06-24 10:05:28 +0200

    add flash form

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -16,7 +16,7 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 /**
  * Require ROLE_ADMIN for *every* controller method in this class.
  *
- * 
+ *
  **/
 class RegistrationController extends AbstractController
 {
