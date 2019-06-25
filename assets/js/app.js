2a3f987cba8a32fdae6460e40574b3c28c185192
mohtth <48013101+mohtth@users.noreply.github.com>
2019-06-25 14:03:26 +0200

    Update app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -9,7 +9,7 @@
 require('../scss/app.scss');
 
 // Need jQuery? Install it with "yarn add jquery", then uncomment to require it.
-const $ = require('jquery');
+//const $ = require('jquery');
 
 console.log('Hello Webpack Encore! Edit me in assets/js/app.js');
 //var $ = require('jquery');
