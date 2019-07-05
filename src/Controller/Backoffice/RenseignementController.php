8c41162a71305240958efc0e754e3d9f90f58c59
Manuel Konrath <manuel.konrath@gmail.com>
2019-07-05 11:51:58 +0200

    add crud contact simple

--- src/Controller/Backoffice/RenseignementController.php
+++ src/Controller/Backoffice/RenseignementController.php
@@ -4,6 +4,7 @@ namespace App\Controller\Backoffice;
 
 use App\Entity\Renseignement;
 use App\Entity\ContactSimple;
+use App\Form\ContactType;
 use App\Form\RenseignementType;
 use App\Repository\ContactSimpleRepository;
 use App\Repository\RenseignementRepository;
@@ -21,8 +22,10 @@ class RenseignementController extends AbstractController
     /**
      * @Route("/", name="renseignement_index", methods={"GET"})
      */
-    public function index(RenseignementRepository $renseignementRepository, ContactSimpleRepository $contactSimpleRepository): Response
+    public function index(ContactSimpleRepository $contactSimpleRepository): Response
     {
+        $renseignementRepository  = $this->getDoctrine()
+            ->getRepository(Renseignement::class);
         return $this->render('Backoffice/renseignement/index.html.twig', [
             'renseignements' => $renseignementRepository->findAll(),
             'contactsSimples' => $contactSimpleRepository->findAll(),
@@ -54,12 +57,47 @@ class RenseignementController extends AbstractController
     }
 
     /**
+     * @Route("/new/Contact", name="contactSimple_new", methods={"GET","POST"})
+     */
+    public function newContactSimple(Request $request): Response
+    {
+        $contactSimple = new ContactSimple();
+        $form = $this->createForm(ContactType::class, $contactSimple);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $contactSimple->setDateMessage(new DateTime());
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($contactSimple);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('renseignement_index');
+        }
+
+        return $this->render('Backoffice/renseignement/newContactSimple.html.twig', [
+            'contactSimple' => $contactSimple,
+            'form' => $form->createView(),
+        ]);
+    }
+
+
+    /**
      * @Route("/{id}", name="renseignement_show", methods={"GET"})
      */
     public function show(Renseignement $renseignement): Response
     {
         return $this->render('Backoffice/renseignement/show.html.twig', [
             'renseignement' => $renseignement,
+        ]);
+    }
+
+    /**
+     * @Route("/contact/{id}", name="contactSimple_show", methods={"GET"})
+     */
+    public function showContact(ContactSimple $contactSimple): Response
+    {
+        return $this->render('Backoffice/renseignement/showContactSimple.html.twig', [
+            'contactSimple' => $contactSimple,
         ]);
     }
 
@@ -87,6 +125,29 @@ class RenseignementController extends AbstractController
     }
 
     /**
+     * @Route("/{id}/editContact", name="contactSimple_edit", methods={"GET","POST"})
+     */
+    public function editContact(Request $request, ContactSimple $contactSimple): Response
+    {
+        $form = $this->createForm(ContactType::class, $contactSimple);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $contactSimple->setDateMessage(new DateTime());
+            $this->getDoctrine()->getManager()->flush();
+
+            return $this->redirectToRoute('renseignement_index', [
+                'id' => $contactSimple->getId(),
+            ]);
+        }
+
+        return $this->render('Backoffice/renseignement/editContactSimple.html.twig', [
+            'contactSimple' => $contactSimple,
+            'form' => $form->createView(),
+        ]);
+    }
+
+    /**
      * @Route("/{id}", name="renseignement_delete", methods={"DELETE"})
      */
     public function delete(Request $request, Renseignement $renseignement): Response
@@ -94,6 +155,20 @@ class RenseignementController extends AbstractController
         if ($this->isCsrfTokenValid('delete'.$renseignement->getId(), $request->request->get('_token'))) {
             $entityManager = $this->getDoctrine()->getManager();
             $entityManager->remove($renseignement);
+            $entityManager->flush();
+        }
+
+        return $this->redirectToRoute('renseignement_index');
+    }
+
+    /**
+     * @Route("/contact/{id}", name="contactSimple_delete", methods={"DELETE"})
+     */
+    public function deleteContact(Request $request, ContactSimple $contactSimple): Response
+    {
+        if ($this->isCsrfTokenValid('delete'.$contactSimple->getId(), $request->request->get('_token'))) {
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->remove($contactSimple);
             $entityManager->flush();
         }
 
