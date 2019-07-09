e2dda13b776d721881db59086d6d123b0732241c
Manuel Konrath <manuel.konrath@gmail.com>
2019-07-09 15:12:52 +0200

    add form pour edit

---
+++ src/Form/PartenaireEditType.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Partenaire;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+
+class PartenaireEditType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('nom')
+            ->add('urlLogo', FileType::class, [
+                'label' => 'Image (jpg, png)',
+                'mapped'=> false,
+                'required' => false,
+                'data_class' => null,
+            ])
+            ->add('visible')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Partenaire::class,
+        ]);
+    }
+}
