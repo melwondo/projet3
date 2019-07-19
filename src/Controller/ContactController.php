90e6a3e1486ff89392b6e8a82bb54a7c9a34a00c
Manuel Konrath <manuel.konrath@gmail.com>
2019-07-19 11:11:01 +0200

    add captcha sur formulaires

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -23,7 +23,7 @@ class ContactController extends AbstractController
      */
     public function index(Request $request, \Swift_Mailer $mailer)
     {
-
+        $captcha = null;
         $blocsPage = $this->getDoctrine()
             ->getRepository(GestionPage::class)
             ->findBy(['PageAssociee'=>'Contact', 'Visible'=>1]);
@@ -34,33 +34,60 @@ class ContactController extends AbstractController
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
-            $eM = $this->getDoctrine()->getManager();
-            $contact->setDateMessage(new DateTime());
-            
-            $tete = $contact->getNom(). ' ' . $contact->getPrenom();
-            $message = (new \Swift_Message())
-            ->setSubject($tete)
-            ->setFrom($this->getParameter('mailer_from'))
-            ->setTo('hhggaamlk@gmail.com')
-            ->setBody(
-                $this->renderView(
-                    'Email/notificationContact.html.twig',
-                    ['client' => $contact]
-                ),
-                'text/html'
-            );
-            
-            $mailer->send($message);
+            //recaptcha
+            if (isset($_POST['g-recaptcha-response'])) {
+                $captcha=$_POST['g-recaptcha-response'];
+            }
+            if (!$captcha) {
+                $this->addFlash(
+                    'danger',
+                    'Veuillez verifier le formulaire captcha'
+                );
+                return $this->redirect($request->getUri());
+            }
+            $secretKey = "6Lfm2KwUAAAAABOwiaCycthCaEIt68JdbJYNZc8R";
+            $ip = $_SERVER['REMOTE_ADDR'];
+            // post request to server
+            $url = 'https://www.google.com/recaptcha/api/siteverify?secret=' .
+                urlencode($secretKey) .  '&response=' . urlencode($captcha);
+            $response = file_get_contents($url);
+            $responseKeys = json_decode($response, true);
+            // should return JSON with success as true
+            if ($responseKeys["success"]) {
+                $eM = $this->getDoctrine()->getManager();
+                $contact->setDateMessage(new DateTime());
 
-            $this->addFlash(
-                'notice',
-                'Votre message a bien été envoyé !'
-            );
+                $tete = $contact->getNom() . ' ' . $contact->getPrenom();
+                $message = (new \Swift_Message())
+                    ->setSubject($tete)
+                    ->setFrom($this->getParameter('mailer_from'))
+                    ->setTo('hhggaamlk@gmail.com')
+                    ->setBody(
+                        $this->renderView(
+                            'Email/notificationContact.html.twig',
+                            ['client' => $contact]
+                        ),
+                        'text/html'
+                    );
 
-            $eM->persist($contact);
-            $eM->flush();
+                $mailer->send($message);
 
-            return $this->redirect($request->getUri());
+                $this->addFlash(
+                    'notice',
+                    'Votre message a bien été envoyé !'
+                );
+
+                $eM->persist($contact);
+                $eM->flush();
+
+                return $this->redirect($request->getUri());
+            } else {
+                $this->addFlash(
+                    'danger',
+                    'Veuillez verifier le formulaire captcha'
+                );
+                return $this->redirect($request->getUri());
+            }
         }
 
         return $this->render('contact/index.html.twig', [
